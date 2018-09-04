a2d754d2381b385d539fe2022d45d9ecf23c7be4
Ender Doe <ender@shpazzle.com>
2018-09-04 19:40:21 -0400

    More package work

---
+++ index.ts
@@ -0,0 +1,9 @@
+import { compose } from 'redux';
+
+// const reduxStore = createStore<StoreState, Action, any, any>(
+//   (rootReducer,
+//   {},
+//   reduxMiddleware
+// );
+
+console.log(compose);
