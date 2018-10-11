1d2ce143516ace3cebe86538bff0a2add5dd3d7d
Ender Doe <ender@shpazzle.com>
2018-10-10 20:46:59 -0400

    Removed redundant files

--- src/redux-muck/utilities.ts
+++
@@ -1,8 +0,0 @@
-import { Subscription } from './index'
-
-export const createHomogenousSelectorSubscription = <T, Result>(selector: (input: T) => Result): Subscription<T, Result> => {
-  return {
-    callback: (A: ReturnType<typeof selector>) => A,
-    selector: selector,
-  }
-};
